7c6a85bff65bfd9bc19b64e485470828449c8793
rrrr_wys@163.com <rrrr_wys@163.com>
2020-07-01 20:50:44 +0800

    my third commit

--- A.cpp
+++ A.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
-using namespace std;
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define per(i,a,b) for(int i=(a);i>=(b);--i)
+#define pb push_back
+#define fi first
+#define se second
+#define Pii pair<int,int>
+#define Pip pair<int,Pii>
+#define Ppi pair<Pii,int>
+#define VI vector<int>
+#define VII vector<Pii>
+typedef long long ll;
+
 int main(){
-    cout << "Hello Git ! Hello Gitee !" << endl;
+    std::cout << "Hello Git ! Hello Gitee !" << std::endl;
     return 0;
 }
